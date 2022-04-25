8f31c3f74b99c219fb3ba58c8466194afb95afaf
JOSE CARLOS MEDEIROS BARROS <102493571+M-barrosj@users.noreply.github.com>
2022-04-24 21:02:42 -0300

    Update Papagaio.java

--- Interface_Segregation_Principle/src/Interface_Segregation_Principle_Correto/Papagaio.java
+++ Interface_Segregation_Principle/src/Interface_Segregation_Principle_Correto/Papagaio.java
@@ -1,11 +1,25 @@
 package Interface_Segregation_Principle_Correto;
 
-public class Papagaio implements AvesQueVoam{
+public class Papagaio implements AvesQueVoam, Aves{
 
 	@Override
 	public void setAltitude() {
 		// TODO Auto-generated method stub
 		
 	}
+
+	@Override
+	public void setLocalizacao() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void renderizar() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 
 }
